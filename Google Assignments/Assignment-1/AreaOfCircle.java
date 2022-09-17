b371a80b166b2ad5c3241eade4e03cb4c9bae4d1
Pavithra-Devi-B <84261310+Pavithra-Devi-B@users.noreply.github.com>
2022-09-17 15:16:13 +0530

    Areaof

---
+++ Google Assignments/Assignment-1/AreaOfCircle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int radius;
+		 radius = sc.nextInt();
+		 double area;
+		 double pi = 3.14;
+		 area =  pi * r * r;
+		 System.out.println("Area of circle"+(int)area);
+	}
+}
