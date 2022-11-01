a766d7a57b99933bf4ee0b3697ed58a6b7d8c223
Pavithra-Devi-B <84261310+Pavithra-Devi-B@users.noreply.github.com>
2022-11-01 14:32:28 +0530

    Move zeroes

---
+++ Question bank/11.Move zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int start=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[start];
+                nums[start]=nums[i];
+                nums[i]=temp;
+                start++;
+            }
+        }
+    }
+}
