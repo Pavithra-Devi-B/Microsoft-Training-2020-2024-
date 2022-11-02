4b10ff82bd9f9fb1f0a601fab1e8779dd987366d
Pavithra-Devi-B <84261310+Pavithra-Devi-B@users.noreply.github.com>
2022-11-02 10:25:26 +0530

    Leetcode 50

---
+++ Question bank/Pow(x, n).java
@@ -0,0 +1,14 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0 || x == 1 || n == 1) {
+            return x;
+        }
+        if (n < 0) {
+            return (1 / x) * myPow(1 / x, -(n + 1));
+        }
+        return n % 2 == 0 ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+    }
+}
